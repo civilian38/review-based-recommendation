4352405f47df2126b074909e53a980127e073f58
CurlyKKoma <toplucky125@gmail.com>
2024-05-06 19:51:52 +0900

    refact/add_description

--- crawling/crawling_module.py
+++ crawling/crawling_module.py
@@ -1,205 +1,148 @@
-# Path: crawling_model.py
-# python crawling_model.py --id [아이디] --pw [비밀번호] --subject [과목명] --professor [교수명]
+# python crawling/crawling_module.py --id [아이디] --pw [비밀번호] --subject [과목명] --out [파일이름]
+# 파일 이름은 .csv 없이 입력
+# 한글 인코딩 형식 cp949
+# 과목 교수 년도 학기 강의평 순으로 저장
+# crawling/data/file.csv에 저장됌
+# Selenium 4.6부터 사용 가능
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from functions import scroll_to_bottom
+import random
+from functions import scroll_down_box_to_bottom
 from selenium.webdriver.chrome.service import Service
 import re, time, json
-from pandas import DataFrame
-import pandas as pd
-import numpy as np
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import argparse
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from selenium.webdriver.common.action_chains import ActionChains
+import csv
+
+semester_dictionary = {'1' : 'Spring', '2': 'Fall', '여' : 'Summer', '겨' : 'Winter'}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--id', required=True)
 parser.add_argument('--pw', required=True)
+
 parser.add_argument('--subject', required=True)
-parser.add_argument('--professor', required=True)
+parser.add_argument('--out', required=True)
+#parser.add_argument('--professor', required=True)
 
 
 my_id = parser.parse_args().id
 my_pw = parser.parse_args().pw
+output_file_path = 'crawling/data/'+parser.parse_args().out+'.csv'
 
-ser = Service(EdgeChromiumDriverManager().install())
 #드라이버 객체 생성
-browser = webdriver.Edge(service=ser)
-#에브리타임 주소 입력
-browser.get('https://everytime.kr')
-
-# Edge 웹 드라이버 설치 및 인스턴스 생성
-ser = Service(EdgeChromiumDriverManager().install())
-browser = webdriver.Edge(service=ser)
-
-# 웹 페이지 열기
-browser.get('https://everytime.kr')
-
-# "메뉴 열기" 버튼 클릭
-menu_button = WebDriverWait(browser, 10).until(
+driver = webdriver.Chrome()
+#웹 페이지 열기
+driver.get('https://everytime.kr')
+#메뉴 열기
+menu_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '/html/body/div/main/header/button'))
 )
 menu_button.click()
-
-# "로그인" 링크 클릭
-login_link = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '/html/body/div/aside/div[2]/div[1]/a[1]'))
+#로그인 창 열기
+open_login_button = WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,'/html/body/div/aside/div[2]/div[1]/a[1]'))
 )
-login_link.click()
-
-
-id_input = WebDriverWait(browser, 3).until(
-    EC.presence_of_element_located((By.NAME, 'id'))
+open_login_button.click()
+#ID, PW 넣어서 로그인
+id_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/form/div[1]/input[1]'))
 )
 id_input.send_keys(my_id)
 
-pw_input = WebDriverWait(browser, 3).until(
-    EC.presence_of_element_located((By.NAME, 'password'))
+pw_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/form/div[1]/input[2]'))
 )
 pw_input.send_keys(my_pw)
 
-submit_button = WebDriverWait(browser, 3).until(
-    EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/form/input'))
+login_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/form/input'))
 )
-submit_button.click()
+login_button.click()
 
-# 강의실 버튼 클릭
-classroom_button = WebDriverWait(browser, 20).until(
-    EC.element_to_be_clickable((By.XPATH, '/html/body/nav/div/ul/li[3]/a'))
+#강의실 버튼 클릭
+classroom_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="menu"]/li[3]/a'))
 )
 classroom_button.click()
 
-# 강좌 검색
+#강좌 검색
 my_subject_input = parser.parse_args().subject
-my_subject_professor = parser.parse_args().professor
 
-subject_input = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[1]/div/form/input[1]'))
+subject_searchBox = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,'/html/body/div/div/div[1]/div/form/input[1]'))
 )
+subject_searchBox.send_keys(my_subject_input)
 
-subject_input.send_keys(my_subject_input)
-
-search_button = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[1]/div/form/input[2]'))
+subject_searchButton = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/div[1]/div/form/input[2]'))
 )
-search_button.click()
+subject_searchButton.click()
 
+scroll_to_bottom(driver)
 
-# 교수 이름을 html에서 찾아서, 해당 div의 name을 찾아서 그 부분을 클릭
-# 해당 교수의 강의가 여러개일 수 있으니까 이를 list로 받아서 클릭할 수 있게 구현
-# 교수 이름을 입력하면 해당 교수의 강의를 모두 찾아서 해당 링크를 미리 저장해놓고, 이를 클릭하는 방식으로 구현
-def find_professor(professor_name):
-    professor_list = browser.find_elements(By.CLASS_NAME, 'professor')
-    professor_link_list = []
-    for professor in professor_list:
-        if professor.text == professor_name:
-            professor_link_list.append(professor)
-    return professor_link_list
+#검색된 강좌의 강의평 링크들 저장
+lectures_url_list = []
 
-def scroll_to_bottom(browser, max_attempts=3):
-    last_height = browser.execute_script("return document.body.scrollHeight")
-    attempts = 0  # 시도 횟수 카운터
-
-    while True:
-        # 스크롤 다운
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)  # 페이지 로드 대기 시간
-
-        try:
-            # 스크롤이 더이상 변화가 없는지 확인
-            WebDriverWait(browser, 10).until(
-                lambda browser: browser.execute_script("return document.body.scrollHeight") > last_height
-            )
-            new_height = browser.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-        except TimeoutException:
-            attempts += 1
-            if attempts >= max_attempts: # 시도 횟수 초과 시 종료
-                break
-
-            print("Waiting a bit longer for page to load...")
-            time.sleep(5)  # 추가 대기 시간
-
-
-# 스크롤 맨 아래로 내리기
-scroll_to_bottom(browser)
-# 로딩 시간 대기
-WebDriverWait(browser, 5).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, 'professor'))
-)
-
-def safe_click(element):
-    try:
-        # 요소가 클릭 가능할 때까지 대기
-        WebDriverWait(browser, 10).until(EC.element_to_be_clickable(element))
-        # 요소 클릭
-        element.click()
-    except ElementClickInterceptedException:
-        # 요소가 화면에 보이도록 스크롤
-        browser.execute_script("arguments[0].scrollIntoView(true);", element)
-        time.sleep(1)  # 잠시 대기 후 클릭
-        # 다시 클릭 시도
-        element.click()
-    except Exception as e:
-        # JavaScript를 사용한 클릭
-        browser.execute_script("arguments[0].click();", element)
-# 교수 이름 클릭
-professor_link_list = find_professor(my_subject_professor)
-if professor_link_list:
-    safe_click(professor_link_list[0])
-
-# 강의평 더보기 클릭
-more_review_button = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[2]/div/section[2]/div[3]/a'))
-)
-more_review_button.click()
-
-# 스크롤 맨 아래로 내리기
-scroll_to_bottom(browser, max_attempts=5)
-
-def get_reviews(browser):
-    WebDriverWait(browser, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'article'))
+try:
+    lectures = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME,'lecture'))
     )
-    review_elements = browser.find_elements(By.CLASS_NAME, 'article')
-    reviews = []
 
-    for element in review_elements:
-        try:
-            # 텍스트 요소 대기 및 찾기
-            text_element = WebDriverWait(element, 5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'text'))
-            )
-            review_text = text_element.text
+    for lecture in lectures:
+        lectures_url_list.append(lecture.get_attribute('href'))
 
-            # 별점 추출
-            star_element = element.find_element(By.CSS_SELECTOR, '.star .on')
-            star_style = star_element.get_attribute('style')
-            star_percentage = re.findall(r'width: (\d+)%;', star_style)[0]
-            #
+except:
+    print("No Such Subject Found")
 
-            reviews.append({'text': review_text, 'stars': int(star_percentage)})
-        except Exception as e:
-            print(f"Error processing review element: {str(e)}")
-            continue
-
-    return reviews
+#순서대로 링크 방문해서 강의평 크롤링
+for lecture_url in lectures_url_list:
+    #에타 정지를 피하려면..
+    time.sleep(random.randint(3,7))
+    driver.get(lecture_url)
 
 
-reviews = get_reviews(browser)
+    lecture_name = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,'/html/body/div/div/div[2]/div/section[1]/div[1]/a/span'))
+    ).text
 
-# review를 json 파일로 저장
-with open(my_subject_input + '_' + my_subject_professor + '.json', 'w', encoding='utf-8') as f:
-    json.dump(reviews, f, ensure_ascii=False, indent=4)
+    try:
+        professor = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[2]/div/section[1]/div[2]/div/a/span'))
+        ).text
+    except:
+        professor = 'Not Determined'
 
-# review를 pandas DataFrame으로 변환
-# df = DataFrame(reviews)
-# df.to_csv(my_subject_input + '_' + my_subject_professor + '.csv', index=False, encoding='utf-8-sig')
+    show_reviews_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[1]/div[2]/a[2]'))
+    )
+    show_reviews_button.click()
+    WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,'/html/body/div/div/div[2]/div/div[2]'))
+    )
+    #모든 리뷰가 로딩될 때까지 scroll down
+    scroll_down_box_to_bottom(driver,'/html/body/div/div/div[2]/div/div[2]')
 
-# 브라우저 종료
-browser.quit()
+    review_tab = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'articles'))
+    )
+
+    reviews = review_tab.find_elements(By.CLASS_NAME,'article')
+
+    # 인코딩 형식 cpc949로 해야 안 한글이 안 깨지네요
+    with open(output_file_path, mode = 'w', encoding= 'cp949') as f:
+        csv_writer = csv.writer(f)
+        #각 리뷰별로 별점과 강의평 추출
+        for review in reviews:
+            review_text = review.find_element(By.CLASS_NAME,'text').text
+            year_semester = review.find_element(By.CLASS_NAME,'article_header').find_element(By.CLASS_NAME,'title').find_element(By.CLASS_NAME,'info').find_element(By.CLASS_NAME,'semester').text
+            year = year_semester[0:2]
+            semester = semester_dictionary[year_semester[4]]
+            csv_writer.writerow([lecture_name,professor,year,semester,review_text])
+
+
+
--- crawling/functions.py
+++ crawling/functions.py
@@ -1,30 +1,53 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-def scroll_to_bottom(browser, max_attempts=3):
-    last_height = browser.execute_script("return document.body.scrollHeight")
-    attempts = 0  # 시도 횟수 카운터
+import time
+
+def scroll_down_box_to_bottom(driver, box_xpath):
+    # 특정 상자(DIV) 요소 찾기
+    box_element = driver.find_element(By.XPATH, box_xpath)
+
+    # 현재 상자(DIV) 내의 항목 개수 확인
+    items = []
+    last_count = len(box_element.find_elements(By.XPATH, "./*"))
+
+    # 계속 스크롤하면서 새로운 항목이 로드될 때까지 대기
+    while True:
+        try:
+            # 상자(DIV) 요소를 기준으로 아래로 스크롤
+            driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight);", box_element)
+
+            # 스크롤 후 대기
+            time.sleep(2)  # 필요에 따라 대기 시간 조정
+
+            # 현재 상자(DIV) 내의 항목 개수 확인
+            current_count = len(box_element.find_elements(By.XPATH, "./*"))
+
+            # 새로운 항목이 로드되지 않으면 반복 중지
+            if current_count == last_count:
+                break
+
+            last_count = current_count  # 항목 개수 업데이트
+        except:
+            print("unexpected error during loading reviews")
+
+
+def scroll_to_bottom(driver):
+    # 페이지 높이 구하기
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
-        # 스크롤 다운
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)  # 페이지 로드 대기 시간
-
         try:
-            # 스크롤이 더이상 변화가 없는지 확인
-            WebDriverWait(browser, 10).until(
-                lambda browser: browser.execute_script("return document.body.scrollHeight") > last_height
-            )
-            new_height = browser.execute_script("return document.body.scrollHeight")
+            # 페이지 아래로 스크롤
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # 페이지 로딩 대기
+            time.sleep(2)  # 필요에 따라 대기 시간 조정
+
+            # 새로운 페이지 높이 구하기
+            new_height = driver.execute_script("return document.body.scrollHeight")
+
+            # 더 이상 스크롤할 수 없으면 반복 종료
             if new_height == last_height:
                 break
             last_height = new_height
         except:
-            attempts += 1
-            if attempts >= max_attempts: # 시도 횟수 초과 시 종료
-                break
-
-            print("Waiting a bit longer for page to load...")
-            time.sleep(5)  # 추가 대기 시간
+            print('unexpected error during loading lectures')
