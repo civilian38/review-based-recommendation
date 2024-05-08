3bac419462c998afc944a7d600abb93ec342602f
CurlyKkoma <137506272+CurlyKkoma@users.noreply.github.com>
2024-05-08 19:13:45 +0900

    Feat/crawling (#3)
    * feat/crawling_module
    
    * refact/create-crawling/data-directory
    
    * refact/add_description
    
    * refact/get-reviews-from-all-lectures
    
    * fix/crawling_rate
    
    * fix/add parameter to open
    
    set encoding='utf-8'
    
    * refact/extra settings
    
    ---------
    
    Co-authored-by: civilian38 <leekyw0323@snu.ac.kr>

---
+++ crawling/functions.py
@@ -0,0 +1,53 @@
+from selenium.webdriver.common.by import By
+import time
+
+def scroll_down_box_to_bottom(driver, box_xpath):
+    # 특정 상자(DIV) 요소 찾기
+    box_element = driver.find_element(By.XPATH, box_xpath)
+
+    # 현재 상자(DIV) 내의 항목 개수 확인
+    items = []
+    last_count = len(box_element.find_elements(By.XPATH, "./*"))
+
+    # 계속 스크롤하면서 새로운 항목이 로드될 때까지 대기
+    while True:
+        try:
+            # 상자(DIV) 요소를 기준으로 아래로 스크롤
+            driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight);", box_element)
+
+            # 스크롤 후 대기
+            time.sleep(2)  # 필요에 따라 대기 시간 조정
+
+            # 현재 상자(DIV) 내의 항목 개수 확인
+            current_count = len(box_element.find_elements(By.XPATH, "./*"))
+
+            # 새로운 항목이 로드되지 않으면 반복 중지
+            if current_count == last_count:
+                break
+
+            last_count = current_count  # 항목 개수 업데이트
+        except:
+            print("unexpected error during loading reviews")
+
+
+def scroll_to_bottom(driver):
+    # 페이지 높이 구하기
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        try:
+            # 페이지 아래로 스크롤
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # 페이지 로딩 대기
+            time.sleep(2)  # 필요에 따라 대기 시간 조정
+
+            # 새로운 페이지 높이 구하기
+            new_height = driver.execute_script("return document.body.scrollHeight")
+
+            # 더 이상 스크롤할 수 없으면 반복 종료
+            if new_height == last_height:
+                break
+            last_height = new_height
+        except:
+            print('unexpected error during loading lectures')
