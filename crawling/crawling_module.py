bbe6106aa797555b3594ff6af3eca15dff087ea9
CurlyKKoma <toplucky125@gmail.com>
2024-05-11 14:50:35 +0900

    fix/과목 명 정확하게 가져오기 및 별점 없는 강의 생략

--- crawling/crawling_module.py
+++ crawling/crawling_module.py
@@ -95,7 +95,7 @@ try:
 
     for lecture in lectures: # 과목 명에 정확하게 들어맞는, 별점이 있는 강의만 가져오도록
         if lecture.find_element(By.CLASS_NAME,'highlight').text == parser.parse_args().subject:
-            if lecture.find_element(By.CLASS_NAME,'rate').find_element(By.CLASS_NAME,'star').find_element(By.CLASS_NAME,'on').get_attribute('style') != 'width: 0%;':
+            if lecture.find_element(By.CLASS_NAME,'rate').find_element(By.CLASS_NAME,'star').find_element(By.CLASS_NAME,'on').get_attribute('style') != 'width: 0;':
                 lectures_url_list.append(lecture.get_attribute('href'))
 
 except:
